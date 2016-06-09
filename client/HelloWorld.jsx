090842957a4fa778588801e66d966cd14b4da274
madsn <m@madsn.net>
2016-06-09 19:12:11 +0000

    Hello world with react

---
+++ client/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class HelloWorld extends React.Component {
+  render() {
+    return (
+      <h1 className="testing-css">Hello World</h1>
+    );
+  }
+}
