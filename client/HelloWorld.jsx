f917aedf3572ce93f27f39dacd176f707aabd73e
madsn <m@madsn.net>
2016-06-09 19:35:24 +0000

    delete everything

--- client/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class HelloWorld extends React.Component {
-  render() {
-    return (
-      <h1 className="testing-css">Hello World</h1>
-    );
-  }
-}
--- client/main.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import { render } from 'react-dom';
-import HelloWorld from './HelloWorld.jsx';
-
-Meteor.startup(() => {
-  render(<HelloWorld />, document.getElementById('app'));
-});
