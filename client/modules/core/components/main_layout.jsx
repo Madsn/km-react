e186d187ad9da951192993018b2a6d0977769565
madsn <m@madsn.net>
2016-06-09 21:02:43 +0000

    Add script tags to layout component

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -34,41 +34,39 @@ const Layout = ({content = () => null }) => (
           <div className="col-xs-10">
             <p>&copy; KennelMate 2016</p>
           </div>
+          <div className="col-xs-2 text-right">
+            <button className="btn btn-info scrollToTop">To Top</button>
+          </div>
         </div>
       </div>
     </div>
+    <script>
+    {`
+      $(document).on('click.nav','.navbar-collapse.in',function(e) {
+        if( $(e.target).is('a') || $(e.target).is('button')) {
+          if ($('.navbar-toggle').is(':visible')) {
+            $(this).collapse('hide');
+          }
+        }
+      });
+      $(document).ready(function(){
+        //Check to see if the window is top if not then display button
+        $(window).scroll(function(){
+          if ($(this).scrollTop() > 100) {
+          	$('.scrollToTop').fadeIn();
+          } else {
+          	$('.scrollToTop').hide();
+          }
+        });
+        //Click event to scroll to top
+        $('.scrollToTop').click(function(){
+          $('html, body').animate({scrollTop : 0},800);
+            return false;
+        });
+      });
+    `}
+    </script>
   </div>
 );
 
 export default Layout;
-
-/*
-          <div className="col-xs-2 text-right">
-            <button className="btn btn-info scrollToTop">To Top</button>
-          </div>
-      <script>
-        $(document).on('click.nav','.navbar-collapse.in',function(e) {
-          if( $(e.target).is('a') || $(e.target).is('button')) {
-            if ($('.navbar-toggle').is(':visible')) {
-              $(this).collapse('hide');
-            }
-          }
-        });
-        $(document).ready(function(){
-          //Check to see if the window is top if not then display button
-          $(window).scroll(function(){
-            if ($(this).scrollTop() > 100) {
-            	$('.scrollToTop').fadeIn();
-            } else {
-            	$('.scrollToTop').hide();
-            }
-          });
-        
-          //Click event to scroll to top
-          $('.scrollToTop').click(function(){
-            $('html, body').animate({scrollTop : 0},800);
-              return false;
-          });
-        });
-      </script>
-*/
